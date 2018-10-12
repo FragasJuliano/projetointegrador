29b131be9c69b105d69ad2474b0d2d3cd00bb4cb
FragasJuliano <43866013+FragasJuliano@users.noreply.github.com>
2018-10-11 22:17:06 -0300

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,44 @@
+// Função modal Esportes //
+
+       function iniciaModal(modalID) {
+         const modal = document.getElementById(modalID); // identificador da variável constante não pode ser alterado //
+         modal.classList.add('mostrar');
+         modal.addEventListener('click', (e) => {
+           if(e.target.id == modalID || e.target.className == 'fechar') {
+            modal.classList.remove('mostrar');
+           }
+         });
+       }
+
+          const meio = document.querySelector('.meio'); // document. querySelector é usado para selecionar a class no CSS //
+          meio.addEventListener('click', () => iniciaModal('modal-noticias')); 
+          
+// Função  modal Infomrativos //
+
+        function iniciaModal(modalID) {
+           const modal = document.getElementById(modalID);
+           modal.classList.add('mostrar');
+           modal.addEventListener('click', (e) => {
+             if(e.target.id == modalID || e.target.className == 'fechar') {
+              modal.classList.remove('mostrar');
+           }
+         });
+       }
+
+          const ladoD = document.querySelector('.ladoD');
+          ladoD.addEventListener('click', () => iniciaModal('modal-noticias1'));
+
+// Função  modal Clima //
+
+        function iniciaModal(modalID) {
+           const modal = document.getElementById(modalID);
+           modal.classList.add('mostrar');
+           modal.addEventListener('click', (e) => {
+             if(e.target.id == modalID || e.target.className == 'fechar') {
+              modal.classList.remove('mostrar');
+           }
+         });
+       }
+
+          const ladoE = document.querySelector('.ladoE');
+          ladoE.addEventListener('click', () => iniciaModal('modal-noticias3'));
